c2ab7d150fdff1817b8b80d26ae5454ce8cbe90c
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-28 14:37:19 +0100

    feat(routes): ajouter les liaisons de modèle et clé de route pour les produits et catégories
    - Liaison du modèle Product avec la route product dans AppServiceProvider
    - Liaison du modèle Category avec la route category dans AppServiceProvider
    - Définition de la clé de route personnalisée 'slug' pour le modèle Category
    - Activation de l'accès aux routes basées sur les modèles Eloquent
    - Amélioration de la gestion des paramètres de route dynamiques
    - Mise en place des relations entre les routes et les modèles de base de données

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::model('product', Product::class);
+        Route::model('category', Category::class);
     }
 }
