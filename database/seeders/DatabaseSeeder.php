2c857cc4d39e3bc4fc6405622aa29203b5aaf67c
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-28 11:17:57 +0100

    refactor(seeder): remplacer bcrypt par Hash::make pour le hachage du mot de passe
    - Importation de Illuminate\Support\Facades\Hash ajoutée
    - Remplacement de bcrypt() par Hash::make() pour le hachage du mot de passe
    - Mise à jour de la méthode de hachage dans la création de l'utilisateur admin
    - Respect des meilleures pratiques de Laravel pour le hachage des mots de passe
    - Amélioration de la cohérence avec les conventions de Laravel
    - Mise à jour du code pour utiliser les méthodes recommandées du framework

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
-            'password' => bcrypt('password')
+            'password' => Hash::make('password')
         ]);
 
         $this->call([
